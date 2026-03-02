9b3e4c7360b71971707c6237ba951c4a6babb9c3
Paul Tomlinson <paul@javascripting.uk>
2026-03-02 19:21:25 +0000

    new events

--- api/events.js
+++ api/events.js
@@ -22,7 +22,3 @@ export default async function handler(req, res) {
       res.status(500).json({ error: "Failed to fetch events" });
     }
   }
-  /*
-export default async function handler(req, res) {
-  res.status(200).json({ message: "Function is working!" });
-}
