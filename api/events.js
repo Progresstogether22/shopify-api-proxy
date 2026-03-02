efff275facd847c6cc0fea700e1a6490d68f4fa3
Paul Tomlinson <paul@javascripting.uk>
2026-03-02 19:12:56 +0000

    new events

--- api/events.js
+++ api/events.js
@@ -1,3 +1,30 @@
 export default async function handler(req, res) {
+    // API key stored in Vercel environment variables - never exposed
+    const EVENTBRITE_TOKEN = process.env.EVENTBRITE_TOKEN;
+    const ORG_ID = process.env.EVENTBRITE_ORG_ID;
+
+    console.log(EVENTBRITE_TOKEN);
+    console.log(ORG_ID);
+    try {
+      const response = await fetch(
+        `https://www.eventbriteapi.com/v3/organizations/${ORG_ID}/events/?status=live`,
+        {
+          headers: {
+            Authorization: `Bearer ${EVENTBRITE_TOKEN}`,
+          },
+        }
+      );
+  
+      const data = await response.json();
+  
+      // Allow requests from your Shopify store only
+      res.setHeader("Access-Control-Allow-Origin", "https://a-siege-of-herons-ltd.myshopify.com");
+      res.status(200).json(data.events);
+    } catch (err) {
+      res.status(500).json({ error: "Failed to fetch events" });
+    }
+  }
+  /*
+export default async function handler(req, res) {
   res.status(200).json({ message: "Function is working!" });
 }
